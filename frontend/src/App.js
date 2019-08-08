e60caf9f5204643a28c14abb2f7dda7484ffbd01
Iago Bontempo <iagobontempo@hotmail.com>
2019-08-08 11:29:58 -0300

    Frontend project start

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="logo"/>
+    </div>
+  );
+}
+
+export default App;
