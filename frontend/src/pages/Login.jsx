7af82bec9d7dadb0c06dcef63fea5ebbee21edd3
Iago Bontempo <iagobontempo@hotmail.com>
2019-08-08 14:37:21 -0300

    HandleSubmit with history.push at Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import logo from '../assets/logo.svg'
 import { Container } from './LoginStyles'
 
-function Login() {
+function Login({ history }) {
+    const [ username, setUsername ] = useState('')
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(username)
+        history.push('/main')
+    }
+
     return (
         <Container>
-            <form>
+            <form onSubmit={handleSubmit}>
             <img src={logo} alt="logo"/>
-                <input placeholder="Enter your Github user" type="text"/>
+
+                <input placeholder="Enter your Github user" type="text" value={username} 
+                    onChange={e => setUsername(e.target.value)}/>
+
                 <button type="submit" >Submit</button>
             </form>
         </Container>
---
+++ frontend/src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './MainStyles';
+
+export default function Main() {
+  return (
+    <Container>
+        <h1>Hello</h1>
+    </Container>
+  );
+}
