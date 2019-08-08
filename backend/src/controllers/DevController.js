08f44f71232b9e510ce291c06d1c51aaa605f30d
Iago Bontempo <iagobontempo@hotmail.com>
2019-08-08 09:37:02 -0300

    DevController, adding a user using github api to collect data

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const Dev = require('../models/Dev'); // Schema import
 
 module.exports = {
     async store(req, res) {
@@ -7,6 +8,15 @@ module.exports = {
         const response = await axios.get(`https://api.github.com/users/${username}`)
         // console.log(response.data); // Axios always returns the response inside a .data (prop)
 
-        return res.json(response.data)
+        const { name, bio, avatar_url } = response.data
+
+        const dev = await Dev.create({
+            name,
+            user: username,
+            bio,
+            avatar: avatar_url
+        });
+
+        return res.json(dev)
     }
 };
