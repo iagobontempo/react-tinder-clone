3598fe348bff5de05de0dcc55b5649350359a29d
Iago Bontempo <iagobontempo@hotmail.com>
2019-08-08 17:07:13 -0300

    React Native start

---
+++ tinder/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native'; 
+// div = View 
+import { Container } from './styles'
+
+function App() {
+  return (
+    <Container>
+      <Text>Hello!</Text>
+    </Container>
+  );
+};
+
+export default App;
