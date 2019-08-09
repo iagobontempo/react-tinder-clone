e824ebfe4f42636bf2593484eed3ad02e70f4296
Iago Bontempo <iagobontempo@hotmail.com>
2019-08-09 13:21:20 -0300

    Mobile almost end

--- tinder/src/pages/Main.js
+++ tinder/src/pages/Main.js
@@ -27,20 +27,24 @@ export default function Main({ navigation }) {
     loadUsers()
   }, [id])
 
-  async function handleLike(id) {
-    await api.post(`/devs/${id}/likes`, null, { // Second param mus be null, because its used for the body of the request
+  async function handleLike() {
+    const [user, ...rest] = users // Its getting user inside users and passing all other users to "rest"
+    // user its the first array and ...rest will be other arrays
+    await api.post(`/devs/${user._id}/likes`, null, { // Second param mus be null, because its used for the body of the request
       headers: { user: id },
     })
 
-    setUsers(users.filter(user => user._id !== id))
+    setUsers(rest)
   }
 
-  async function handleDislike(id) {
-    await api.post(`/devs/${id}/dislikes`, null, { // Second param mus be null, because its used for the body of the request
+  async function handleDislike() {
+    const [user, ...rest] = users
+
+    await api.post(`/devs/${user._id}/dislikes`, null, { // Second param mus be null, because its used for the body of the request
       headers: { user: id },
     })
 
-    setUsers(users.filter(user => user._id !== id))
+    setUsers(rest)
   }
 
   async function handleLogout() {
@@ -54,29 +58,29 @@ export default function Main({ navigation }) {
         <Logo source={logo} />
       </TouchableOpacity>
       <CardContainer>
-        {users.length === 0 ?
-          <NoUser>Acabou :o</NoUser>
-          :
-          (users.map((user, index) => {
+        {users.length === 0
+          ? <NoUser>Acabou :(</NoUser>
+          : users.map((user, index) => (
             <Card key={user._id} style={{ zIndex: users.length - index }}>
               <CardImage source={{ uri: user.avatar }} />
-              <CardFooter>
+              <CardFooter >
                 <NameText>{user.name}</NameText>
                 <BioText numberOfLines={3}>{user.bio}</BioText>
               </CardFooter>
             </Card>
-          })
-          )}
+          ))}
       </CardContainer>
 
-      <ButtonContainer>
-        <ActionButton>
-          <Image source={dislike} />
-        </ActionButton>
-        <ActionButton>
-          <Image source={like} />
-        </ActionButton>
-      </ButtonContainer>
+      {users.length > 0 && (
+        <ButtonContainer>
+          <ActionButton onPress={handleDislike}>
+            <Image source={dislike} />
+          </ActionButton>
+          <ActionButton onPress={handleLike}>
+            <Image source={like} />
+          </ActionButton>
+        </ButtonContainer>
+      )}
     </Container>
   );
 }
